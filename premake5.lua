460575c1f29da508db99ad076a26b46e388fb5bb
Jonathan Young <young.jpc@gmail.com>
2015-12-01 19:38:51 +1000

    Prep work for building with mingw.

---
+++ premake5.lua
@@ -0,0 +1,77 @@
+if _ACTION == nil then
+	return
+end
+
+local BGFX_PATH = path.join(path.getabsolute(".."), "bgfx")
+
+if not os.isdir(BGFX_PATH) then
+	print("bgfx not found at " .. BGFX_PATH)
+	os.exit()
+end
+
+local BX_PATH = path.join(path.getabsolute(".."), "bx")
+
+if not os.isdir(BX_PATH) then
+	print("bx not found at " .. BX_PATH)
+	os.exit()
+end
+
+local IOQ3_PATH = path.join(path.getabsolute(".."), "ioq3")
+
+if not os.isdir(IOQ3_PATH) then
+	print("ioquake3 not found at " .. IOQ3_PATH)
+	os.exit()
+end
+
+local RENDERER_PATH = path.getabsolute(".")
+	
+if os.get() == "windows" then
+	os.mkdir("build")
+	os.mkdir("build/bin_x86")
+	os.mkdir("build/bin_x64")
+	os.mkdir("build/bin_debug_x86")
+	os.mkdir("build/bin_debug_x64")
+	os.copyfile("D3DCompiler_47.dll", "build/bin_x86/D3DCompiler_47.dll")
+	os.copyfile("D3DCompiler_47.dll", "build/bin_x64/D3DCompiler_47.dll")
+	os.copyfile("D3DCompiler_47.dll", "build/bin_debug_x86/D3DCompiler_47.dll")
+	os.copyfile("D3DCompiler_47.dll", "build/bin_debug_x64/D3DCompiler_47.dll")
+end
+
+solution "renderer_bgfx"
+	language "C"
+	location "build"
+	startproject "renderer_bgfx"
+	platforms { "native", "x32", "x64" }
+	configurations { "Debug", "Release" }
+	
+	configuration "vs*"
+		defines { "_CRT_SECURE_NO_DEPRECATE" }
+	
+	configuration "x64"
+		defines { "_WIN64", "__WIN64__" }
+			
+	configuration "Debug"
+		optimize "Debug"
+		defines { "_DEBUG" }
+		flags "Symbols"
+				
+	configuration "Release"
+		optimize "Full"
+		defines "NDEBUG"
+		
+	configuration { "Debug", "not x64" }
+		targetdir "build/bin_debug_x86"
+		
+	configuration { "Release", "not x64" }
+		targetdir "build/bin_x86"
+		
+	configuration { "Debug", "x64" }
+		targetdir "build/bin_debug_x64"
+		
+	configuration { "Release", "x64" }
+		targetdir "build/bin_x64"
+
+dofile("renderer_bgfx.lua")
+createBgfxProject(BGFX_PATH, BX_PATH)
+createRendererProject(BGFX_PATH, BX_PATH, IOQ3_PATH, RENDERER_PATH, path.join(IOQ3_PATH, "code/libs/win32/libSDL2"), path.join(IOQ3_PATH, "code/libs/win64/libSDL264"))
+createShadercProject(BGFX_PATH, BX_PATH, RENDERER_PATH)
--- renderer_bgfx.lua
+++ renderer_bgfx.lua
@@ -1,36 +1,54 @@
-local IOQ3_PATH = path.join(path.getabsolute(".."), "ioq3")
+function createBgfxProject(bgfxPath, bxPath)
+	project "bgfx"
+	language "C++"
+	kind "StaticLib"
+	
+	files
+	{
+		path.join(bgfxPath, "src/*.cpp"),
+		path.join(bgfxPath, "src/*.h"),
+		path.join(bgfxPath, "include/*.h")
+	}
+	
+	excludes
+	{
+		path.join(bgfxPath, "src/amalgamated.cpp"),
+		path.join(bgfxPath, "src/ovr.cpp"),
+		path.join(bgfxPath, "src/glcontext_ppapi.cpp"),
+		path.join(bgfxPath, "src/glcontext_glx.cpp"),
+		path.join(bgfxPath, "src/glcontext_egl.cpp")
+	}
+	
+	includedirs
+	{
+		path.join(bgfxPath, "include"),
+		path.join(bgfxPath, "3rdparty"),
+		path.join(bgfxPath, "3rdparty/dxsdk/include"),
+		path.join(bgfxPath, "3rdparty/khronos"),
+		bxPath,
+		path.join(bxPath, "include"),
+	}
+	
+	configuration "vs*"
+		includedirs { path.join(bxPath, "include/compat/msvc") }
+	configuration { "windows", "gmake" }
+		defines { "WIN32", "_WIN32" }
+		includedirs { path.join(bxPath, "include/compat/mingw") }
+	configuration {}
+	
+	defines { "__STDC_FORMAT_MACROS" }
 
-if not os.isdir(IOQ3_PATH) then
-	print("ioquake3 not found at " .. IOQ3_PATH)
-	os.exit()
+	configuration "Debug"
+		defines { "BGFX_CONFIG_DEBUG=1" }
 end
 
-local IOQ3_PREMAKE_MSVC_PATH = path.join(path.getabsolute(".."), "ioq3-premake-msvc")
 
-if not os.isdir(IOQ3_PREMAKE_MSVC_PATH) then
-	print("ioq3-premake-msvc not found at " .. IOQ3_PREMAKE_MSVC_PATH)
-	os.exit()
-end
-
-local BX_PATH = path.join(path.getabsolute(".."), "bx")
-
-if not os.isdir(BX_PATH) then
-	print("bx not found at " .. BX_PATH)
-	os.exit()
-end
-
-local BGFX_PATH = path.join(path.getabsolute(".."), "bgfx")
-
-if not os.isdir(BGFX_PATH) then
-	print("bgfx not found at " .. BGFX_PATH)
-	os.exit()
-end
-
-project "renderer_bgfx"
+function createRendererProject(bgfxPath, bxPath, ioq3Path, rendererPath, sdlLib32, sdlLib64)
+	project "renderer_bgfx"
 	language "C++"
 	kind "SharedLib"
 	pchheader "Precompiled.h"
-	pchsource "code/renderer_bgfx/Precompiled.cpp"
+	pchsource(path.join(rendererPath, "code/renderer_bgfx/Precompiled.cpp"))
 	
 	configuration "x64"
 		targetname "renderer_bgfx_x86_64"
@@ -57,46 +75,46 @@ project "renderer_bgfx"
 		"%{prj.location}/dynamic/renderer_bgfx/TextureColor_fragment.c",
 		"%{prj.location}/dynamic/renderer_bgfx/TextureColor_vertex.c",
 		"%{prj.location}/dynamic/renderer_bgfx/varying_def.c",
-		"code/qcommon/cm_public.h",
-		"code/qcommon/puff.c",
-		"code/qcommon/puff.h",
-		"code/qcommon/q_math.c",
-		"code/qcommon/q_platform.h",
-		"code/qcommon/q_shared.c",
-		"code/qcommon/q_shared.h",
-		"code/qcommon/qcommon.h",
-		"code/qcommon/qfiles.h",
-		"code/qcommon/surfaceflags.h",
-		"code/math/*.cpp",
-		"code/math/*.h",
-		"code/renderer_bgfx/*.cpp",
-		"code/renderer_bgfx/*.h",
-		"code/renderercommon/qgl.h",
-		"code/renderercommon/tr_common.h",
-		"code/renderercommon/tr_noise.c",
-		"code/renderercommon/tr_public.h",
-		"code/renderercommon/tr_types.h",
-		"shaders/*.sc",
-		"shaders/*.sh"		
+		path.join(rendererPath, "code/qcommon/cm_public.h"),
+		path.join(rendererPath, "code/qcommon/puff.c"),
+		path.join(rendererPath, "code/qcommon/puff.h"),
+		path.join(rendererPath, "code/qcommon/q_math.c"),
+		path.join(rendererPath, "code/qcommon/q_platform.h"),
+		path.join(rendererPath, "code/qcommon/q_shared.c"),
+		path.join(rendererPath, "code/qcommon/q_shared.h"),
+		path.join(rendererPath, "code/qcommon/qcommon.h"),
+		path.join(rendererPath, "code/qcommon/qfiles.h"),
+		path.join(rendererPath, "code/qcommon/surfaceflags.h"),
+		path.join(rendererPath, "code/math/*.cpp"),
+		path.join(rendererPath, "code/math/*.h"),
+		path.join(rendererPath, "code/renderer_bgfx/*.cpp"),
+		path.join(rendererPath, "code/renderer_bgfx/*.h"),
+		path.join(rendererPath, "code/renderercommon/qgl.h"),
+		path.join(rendererPath, "code/renderercommon/tr_common.h"),
+		path.join(rendererPath, "code/renderercommon/tr_noise.c"),
+		path.join(rendererPath, "code/renderercommon/tr_public.h"),
+		path.join(rendererPath, "code/renderercommon/tr_types.h"),
+		path.join(rendererPath, "shaders/*.sc"),
+		path.join(rendererPath, "shaders/*.sh")
 	}
 	
-	filter("files:**.c or code/math/*.cpp")
+	filter("files:**.c or " .. path.getrelative(path.getabsolute("."), path.join(rendererPath, "code/math/*.cpp")))
 		flags "NoPCH"
 	filter {}
 	
 	vpaths
 	{
-		["shaders"] = { "shaders/*.sc", "shaders/*.sh" },
-		["*"] = "code"
+		["shaders"] = path.join(rendererPath, "shaders/*.*"),
+		["*"] = path.join(rendererPath, "code")
 	}
 	
 	includedirs
 	{
-		path.join(IOQ3_PATH, "code/SDL2/include"),
-		path.join(BX_PATH, "include"),
-		path.join(BGFX_PATH, "include"),
-		path.join(BGFX_PATH, "src"), -- for bgfx_shader.sh
-		"code/stb"
+		path.join(ioq3Path, "code/SDL2/include"),
+		path.join(bxPath, "include"),
+		path.join(bgfxPath, "include"),
+		path.join(bgfxPath, "src"), -- for bgfx_shader.sh
+		path.join(rendererPath, "code/stb")
 	}
 	
 	links
@@ -110,15 +128,116 @@ project "renderer_bgfx"
 		"shaderc"
 	}
 	
-	configuration "shaders/*.*"
+	configuration(path.getrelative(path.getabsolute("."), path.join(rendererPath, "shaders/*.*")))
 		buildmessage "Stringifying %{file.name}"
-		buildcommands("cscript.exe \"" .. path.join(IOQ3_PATH, "misc/msvc/glsl_stringify.vbs") .. "\" //Nologo \"%{file.relpath}\" \"%{prj.location}\\dynamic\\renderer_bgfx\\%{file.basename}.c\"")
+		buildcommands("cscript.exe \"" .. path.join(ioq3Path, "misc/msvc/glsl_stringify.vbs") .. "\" //Nologo \"%{file.relpath}\" \"%{prj.location}\\dynamic\\renderer_bgfx\\%{file.basename}.c\"")
 		buildoutputs "%{prj.location}\\dynamic\\renderer_bgfx\\%{file.basename}.c"
 	
 	configuration "not x64"
-		links(path.join(IOQ3_PREMAKE_MSVC_PATH, "SDL2/x86/SDL2.lib"))
+		links(sdlLib32)
 	configuration "x64"
-		links(path.join(IOQ3_PREMAKE_MSVC_PATH, "SDL2/x64/SDL2.lib"))
+		links(sdlLib64)
+	configuration "vs*"
+		buildoptions { "/wd\"4316\"", "/wd\"4351\"" } -- Silence some warnings
+end
+
+--
+-- Copyright 2010-2015 Branimir Karadzic. All rights reserved.
+-- License: http://www.opensource.org/licenses/BSD-2-Clause
+--
+function createShadercProject(bgfxPath, bxPath, rendererPath)
+	project "shaderc"
+	language "C++"
+	kind "StaticLib"
+	
+	local GLSL_OPTIMIZER = path.join(bgfxPath, "3rdparty/glsl-optimizer")
+	local FCPP_DIR = path.join(rendererPath, "code/fcpp")
+
+	removeflags
+	{
+		"OptimizeSpeed", -- GCC 4.9 -O2 + -fno-strict-aliasing don't work together...
+	}
+	
+	configuration "vs*"
+		includedirs
+		{
+			path.join(bxPath, "include/compat/msvc"),
+			path.join(GLSL_OPTIMIZER, "src/glsl/msvc")
+		}
+
+		-- glsl-optimizer
+		defines
+		{
+			"__STDC__",
+			"__STDC_VERSION__=199901L",
+			"strdup=_strdup",
+			"alloca=_alloca",
+			"isascii=__isascii"
+		}
+
+		buildoptions
+		{
+			"/wd4291", -- 'declaration' : no matching operator delete found; memory will not be freed if initialization throws an exception
+			"/wd4996" -- warning C4996: 'strdup': The POSIX name for this item is deprecated. Instead, use the ISO C++ conformant name: _strdup.
+		}
+
 	configuration {}
 	
-	buildoptions { "/wd\"4316\"", "/wd\"4351\"" } -- Silence some warnings
+	defines { "SHADERC_LIB" }
+
+	-- fcpp
+	defines
+	{
+		"NINCLUDE=64",
+		"NWORK=65536",
+		"NBUFF=65536",
+		"OLD_PREPROCESSOR=0"
+	}
+
+	includedirs
+	{
+		path.join(bxPath, "include"),
+		path.join(bgfxPath, "include"),
+		path.join(bgfxPath, "tools/shaderc"),
+		FCPP_DIR,
+		path.join(GLSL_OPTIMIZER, "src"),
+		path.join(GLSL_OPTIMIZER, "include"),
+		path.join(GLSL_OPTIMIZER, "src/mesa"),
+		path.join(GLSL_OPTIMIZER, "src/mapi"),
+		path.join(GLSL_OPTIMIZER, "src/glsl"),
+	}
+
+	files
+	{
+		path.join(rendererPath, "code/shaderc/*.cpp"),
+		path.join(rendererPath, "code/shaderc/*.h"),
+		path.join(bgfxPath, "src/vertexdecl.**"),
+		path.join(FCPP_DIR, "**.h"),
+		path.join(FCPP_DIR, "cpp1.c"),
+		path.join(FCPP_DIR, "cpp2.c"),
+		path.join(FCPP_DIR, "cpp3.c"),
+		path.join(FCPP_DIR, "cpp4.c"),
+		path.join(FCPP_DIR, "cpp5.c"),
+		path.join(FCPP_DIR, "cpp6.c"),
+		path.join(FCPP_DIR, "cpp6.c"),
+		path.join(GLSL_OPTIMIZER, "src/mesa/**.c"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/**.cpp"),
+		path.join(GLSL_OPTIMIZER, "src/mesa/**.h"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/**.c"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/**.cpp"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/**.h"),
+		path.join(GLSL_OPTIMIZER, "src/util/**.c"),
+		path.join(GLSL_OPTIMIZER, "src/util/**.h"),
+	}
+
+	removefiles
+	{
+		path.join(GLSL_OPTIMIZER, "src/glsl/glcpp/glcpp.c"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/glcpp/tests/**"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/glcpp/**.l"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/glcpp/**.y"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/ir_set_program_inouts.cpp"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/main.cpp"),
+		path.join(GLSL_OPTIMIZER, "src/glsl/builtin_stubs.cpp"),
+	}
+end
