d67218419b39e91505b128e7a8318d7ce5bd5fb7
Jonathan Young <young.jpc@gmail.com>
2016-06-26 14:12:59 +1000

    Fix mingw build.

--- code/renderer_bgfx/DynamicLight.cpp
+++ code/renderer_bgfx/DynamicLight.cpp
@@ -240,7 +240,7 @@ void DynamicLightManager::updateTextures(int frameNo)
 	{
 		assert(indicesOffset < indicesTextureSize_ * indicesTextureSize_);
 		const uint16_t width = std::min(indicesOffset, indicesTextureSize_);
-		const uint16_t height = (uint16_t)std::ceil(indicesOffset / (float)indicesTextureSize_);
+		const uint16_t height = (uint16_t)ceil(indicesOffset / (float)indicesTextureSize_);
 		bgfx::updateTexture2D(indicesTexture_, 0, 0, 0, width, height, bgfx::makeRef(indicesTextureData_[buffer].data(), indicesOffset));
 	}
 
@@ -251,7 +251,7 @@ void DynamicLightManager::updateTextures(int frameNo)
 		const uint32_t texelSize = sizeof(float) * 4; // RGBA32F
 		const uint16_t nTexels = uint16_t(size / texelSize);
 		const uint16_t width = std::min(nTexels, lightsTextureSize_);
-		const uint16_t height = (uint16_t)std::ceil(nTexels / (float)lightsTextureSize_);
+		const uint16_t height = (uint16_t)ceil(nTexels / (float)lightsTextureSize_);
 		bgfx::updateTexture2D(lightsTexture_, 0, 0, 0, width, height, bgfx::makeRef(lights_[buffer], size));
 	}
 }
