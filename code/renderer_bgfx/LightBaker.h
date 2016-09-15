bf90c2993f8f201d3c9d4c884d7d7b664bc816dc
Jonathan Young <young.jpc@gmail.com>
2016-09-15 14:03:44 +1000

    Missing #pragma once on header.

--- code/renderer_bgfx/LightBaker.h
+++ code/renderer_bgfx/LightBaker.h
@@ -19,6 +19,8 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
+#pragma once
+
 #include "Precompiled.h"
 #pragma hdrstop
 
